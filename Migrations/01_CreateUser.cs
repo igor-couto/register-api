c0261fa9cd9733f067f60b3ea7d4ddbe5d52eb66
igor-couto <igor.fcouto@gmail.com>
2022-11-07 21:31:21 -0300

    fix: Create user migration

--- Migrations/01_CreateUser.cs
+++ Migrations/01_CreateUser.cs
@@ -7,13 +7,19 @@ public class CreateClosedDateTable : Migration
 {
     public override void Up()
     {
-        Create.Table("user")
-            .WithColumn("id").AsDate().NotNullable().PrimaryKey()
-            .WithColumn("email").AsDate().NotNullable()
-            .WithColumn("phoneNumber").AsString(16).NotNullable()
+        Create.Table("users")
+            .WithColumn("id").AsGuid().NotNullable().PrimaryKey()
+            .WithColumn("user_name").AsString(64).NotNullable()
+            .WithColumn("first_name").AsString(64).NotNullable()
+            .WithColumn("last_name").AsString(64).NotNullable()
+            .WithColumn("email").AsString(256).NotNullable()
+            .WithColumn("is_email_confirmed").AsBoolean().NotNullable()
+            .WithColumn("is_locked").AsBoolean().WithDefaultValue(false).NotNullable()
+            .WithColumn("phone_number").AsString(16).NotNullable()
             .WithColumn("created_at").AsDate().NotNullable()
-            .WithColumn("updated_at").AsDate().NotNullable();
+            .WithColumn("updated_at").AsDate().NotNullable()
+            .WithColumn("password_hash").AsString().NotNullable();
     }
 
-    public override void Down() => Delete.Table("user");
+    public override void Down() => Delete.Table("users");
 }
