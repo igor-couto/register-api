7f35d15fce95787f9755828c366ad1644cc9c352
igor-couto <igor.fcouto@gmail.com>
2022-11-07 21:30:07 -0300

    Create and update requests

--- RegisterAPI/Domain/Requests/CreateUserRequest.cs
+++ RegisterAPI/Domain/Requests/CreateUserRequest.cs
@@ -2,9 +2,30 @@ namespace RegisterAPI.Domain.Requests;
 
 public class CreateUserRequest
 {
-    public string UserName;
-    public string FirstName;
-    public string LastName;
-    public string Email;
-    public string PhoneNumber;
+    public string UserName { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Password { get; set; }
+
+    public User Create()
+    {
+        var currentDate = DateTime.Now;
+
+        return new User()
+        {
+            Id = Guid.NewGuid(),
+            UserName = UserName,
+            FirstName = FirstName,
+            LastName = LastName,
+            Email = Email,
+            PhoneNumber = PhoneNumber,
+            IsEmailConfirmed = false,
+            IsLocked = false,
+            CreatedAt = currentDate,
+            UpdatedAt = currentDate,
+            PasswordHash = Password
+        };
+    }
 }
--- RegisterAPI/Domain/Requests/UpdateUserRequest.cs
+++ RegisterAPI/Domain/Requests/UpdateUserRequest.cs
@@ -2,5 +2,24 @@ namespace RegisterAPI.Domain.Requests;
 
 public class UpdateUserRequest
 {
-    public Guid Id;
+    public string UserName { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public bool IsEmailConfirmed { get; set; }
+    public bool IsLocked { get; set; }
+
+    public User Update(User user)
+    {
+        user.UserName = UserName;
+        user.FirstName = FirstName;
+        user.LastName = LastName;
+        user.Email = Email;
+        user.IsEmailConfirmed = IsEmailConfirmed;
+        user.IsLocked = IsLocked;
+        user.UpdatedAt = DateTime.Now;
+
+        return user;
+    }
 }
