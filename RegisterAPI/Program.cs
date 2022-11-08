249a73b3bfb7152e987dbf9dd40907fa9a49614b
igor-couto <igor.fcouto@gmail.com>
2022-11-07 21:49:00 -0300

    Cors, database configurations, health check and swagger configuration

--- RegisterAPI/Program.cs
+++ RegisterAPI/Program.cs
@@ -1,21 +1,56 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using RegisterAPI.Infrastructure;
+
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<DataContext>(options => {
-    options.UseNpgsql(builder.Configuration.GetConnectionString("ConnectionStrings"));
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options
+        .UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+        .UseSnakeCaseNamingConvention();
+});
+
+builder.Services.AddHealthChecks();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(
+        name: "policy",
+        policy =>
+        {
+            policy.WithOrigins("*");
+        });
 });
 
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(config =>
+{
+    config.SwaggerDoc("v0",
+        new OpenApiInfo
+        {
+            Title = "Register API",
+            Version = "v0",
+            Contact = new OpenApiContact { Name = "Igor Couto", Email = "igor.fcouto@gmail.com" }
+        });
+});
 
 var app = builder.Build();
 
+app.UseCors();
+
+app.MapHealthChecks("/health");
+
 app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v0/swagger.json", "Register API v0");
+    options.DefaultModelsExpandDepth(-1);
+});
 
 app.UseHttpsRedirection();
 
